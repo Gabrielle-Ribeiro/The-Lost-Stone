a64fa8d34f11b2c4f3e6b9ac9889e718969669c1
Gabrielle <gabrielleribeiro2010@gmail.com>
2018-05-01 21:00:56 -0300

    Criação do script da câmera e atualização de cenário e background

---
+++ Assets/Code/CameraBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+	// Variáveis usadas na movimentação da câmera
+	public Transform Target; 
+	public Vector2 Offset = new Vector2(9.22f, 1f);
+
+	void Start () {
+		// Pega as coordenadas do GameObject MainChar e guarda na variável Target
+		Target = GameObject.Find ("MainChar").transform;
+		Vector3 targetPosition = this.transform.position;
+		targetPosition.x = Target.position.x + Offset.x;
+		this.transform.position = targetPosition;
+	}
+
+	void Update () {
+		// Faz com que a câmera siga o MainChar
+		Vector3 targetPosition = this.transform.position;
+		targetPosition.x = Target.position.x + Offset.x;
+		this.transform.position = targetPosition;
+	}
+}
