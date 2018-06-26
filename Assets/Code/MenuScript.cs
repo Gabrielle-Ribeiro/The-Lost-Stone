0fc8df9ee449a589e857a1a251970803e7d64ee7
Helena Goulart <helenabretasg@gmail.com>
2018-06-26 19:58:49 -0300

    Inclusao do menu de iniciar

---
+++ Assets/Code/MenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    private void OnGUI()
+    {
+        const int buttonWidth = 84;
+        const int buttonHeight = 60;
+
+        if (GUI.Button(
+            new Rect(Screen.width / 2 - (buttonWidth / 2),
+                     (2 * Screen.height / 3) - (buttonWidth / 2),
+                     buttonWidth, buttonHeight),
+            "Start!"));
+        {
+            Application.LoadLevel(Level1);
+        }
+    }
+}
