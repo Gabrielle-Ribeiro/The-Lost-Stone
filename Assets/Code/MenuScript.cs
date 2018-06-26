c67017c7e12ba169476637d1845ca73b08c11ade
Pedro Jales <pedrojales.eng@outlook.com>
2018-06-26 20:31:35 -0300

    exemplo button adcionado

--- Assets/Code/MenuScript.cs
+++ Assets/Code/MenuScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
@@ -9,13 +10,17 @@ public class Menu : MonoBehaviour {
         const int buttonWidth = 84;
         const int buttonHeight = 60;
 
+        int Level;
+
+        //PlayerPrefs.GetInt("FaseSalva", Level);
+
         if (GUI.Button(
             new Rect(Screen.width / 2 - (buttonWidth / 2),
                      (2 * Screen.height / 3) - (buttonWidth / 2),
                      buttonWidth, buttonHeight),
             "Start!"));
         {
-            Application.LoadLevel(Level1);
+            Application.LoadLevel(1);
         }
     }
 }
